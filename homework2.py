374013850b90a7cd094d339b3b3beea5029f75b7
Nadezhda986 <luna_986@mail.ru>
2024-07-07 23:53:32 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+home_assignments_count= 12
+hours_count= 1.5
+course= "Python"
+time_of_one_task=hours_count/home_assignments_count
+print('Курс:', course, ', всего задач:', home_assignments_count, ', затрачено часов:', hours_count, ', среднее время выполнения', time_of_one_task, 'часа.')
