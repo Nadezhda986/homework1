212b593a9f1d121db58a1915318c1edeedb12619
Nadezhda986 <luna_986@mail.ru>
2024-07-11 22:28:21 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string=input()
+print("количество символов", len(my_string))
+print(my_string.upper())
+print(my_string.upper().lower())
+print(my_string.replace(' ', ''))
+print("Первый символ фразы", my_string[0])
+print("Последний символ фразы", my_string[-1])
