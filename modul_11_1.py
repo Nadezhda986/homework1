9306ae6d757e0f762092b30464bf171261768e9f
Nadezhda986 <luna_986@mail.ru>
2024-11-18 20:57:01 +0300

    Add files via upload

--- modul_11_1.py
+++ modul_11_1.py
@@ -1,3 +1,5 @@
+# библиотека numpy позволяет обрабатывать массив чисел. Преобразовывать  масссивы
+# в матрицу, совершать маематические операции.
 import numpy as np
 
 def print_array(a):
@@ -8,6 +10,30 @@ def summ(a,b,c):
 summ(a=np.array([10,20,30,15]),b=np.array(10),c=20)
 
 
-from PIL import image
-im = image.open ("hopper.ppm")
-print(im.format, im.size, im.mode)
+
+# библиотека pillow позволяет работаь с изображениями.
+# Проводить простые действия без привлечения графических редакторов.
+from PIL import Image
+im1 = Image.open("kartinka.jpg")
+print(im1.format, im1.size, im1.mode)
+im2 = Image.open("LediBag i Superkot.png")
+out = im1.resize((750,750))
+out = im2.resize((750, 750))
+im3 = im2.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+im1.paste(im3, (0,0))
+im1.show()
+new_image = Image.new('RGB',(2*im1.size[0], im1.size[1]), (250,250,250))
+out = new_image.resize((750, 750))
+new_image.paste(im1,(0,0))
+new_image.paste(im2, (im1.size[0], 0))
+new_image.show()
+
+out = im1.transpose(Image.Transpose.ROTATE_90)
+out.show()
+
+
+
+
+
+
+
